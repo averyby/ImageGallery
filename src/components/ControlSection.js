45b16447fcf34d8d463f7820c80a4b19788879fb
Avery By <averyby@yahoo.com>
2017-06-02 23:02:32 +0800

    Use readAsDataURL to process images

--- src/components/ControlSection.js
+++ src/components/ControlSection.js
@@ -11,16 +11,34 @@ export default class ControlSection extends Component {
         togglePlaying(rest);
     };
 
-    getImages = (images) => {
+    getImages = async (images) => {
         if (!images || !images.length) return;
         const { id } = this.props;
-        images = images.map(
-            (img, idx) => ({
+        let imgInfos = [];
+        for(let i = 0; i < images.length; i++) {
+            imgInfos.push(await this.getImageInfo(images[i]));
+        }
+        // imgInfos = images.map(
+        //     async img => {
+        //         let imgInfo = await this.getImageInfo(img);
+        //         debugger;
+        //         return imgInfo;
+        //     }
+        // );
+        this.props.addImages({ id, images: imgInfos });
+    };
+
+    getImageInfo = (img) => {
+        return new Promise((resolve, reject) => {
+            let reader = new FileReader();
+
+            reader.onload = () => resolve({
                 name: img.name,
-                imgUrl: window.URL.createObjectURL(img)
-            })
-        );
-        this.props.addImages({ id, images });
+                imgUrl: reader.result
+            });
+
+            reader.readAsDataURL(img);
+        });
     };
 
     handleDelete = () => this.props.deleteGallery(this.props.id);
