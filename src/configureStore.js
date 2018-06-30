245f2e9246a19930a18bdb93d81dd3609466042f
柏正权 <baizhengquan@zhongan.com>
2018-06-30 10:44:58 +0800

    change default images

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,10 +4,6 @@ import uuidV4 from 'uuid/v4';
 import userGalleries from './reducers/userGalleries';
 import demoGallery from './reducers/demoGallery';
 
-import boy1 from './img/boy1.jpg';
-import boy2 from './img/boy2.jpg';
-import boy3 from './img/boy3.jpg';
-import boy4 from './img/boy4.jpg';
 import fox from './img/Fox.jpg';
 import road from './img/road.jpg';
 import sunset from './img/sunset.jpg';
@@ -24,8 +20,7 @@ export default () => {
 
     return createStore(reducer, {
         demoGallery: {
-            images: [boy1, boy2, boy3, boy4],
-            // images: [fox, road, sunset],
+            images: [fox, road, sunset],
             playing: true,
             id: uuidV4()
         }
